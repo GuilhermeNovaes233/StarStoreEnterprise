96728e60f5c2f32164f348c7d52d2f5cbd4cc138
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-09-02 23:44:08 -0300

    Adicionando IdentityController e Views de Login e Registro

--- src/web/Star.WebApp.MVC/Controllers/IdentityController.cs
+++ src/web/Star.WebApp.MVC/Controllers/IdentityController.cs
@@ -1,16 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Star.WebApp.MVC.Models.User;
 using System.Threading.Tasks;
 
 namespace Star.WebApp.MVC.Controllers
 {
     public class IdentityController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("new-account")]
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost("new-account")]
+        public async Task<IActionResult> Register(UserRegister userRegister)
+        {
+            if (!ModelState.IsValid)
+                return View(userRegister);
+
+            //API - Registro
+            if (false) return View(userRegister);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return View(userLogin);
+
+            //API - Login
+            if (false) return View(userLogin);
+
+            //Realizar Login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            //Limpar Cookie
+            return RedirectToAction("Index", "Home");
         }
     }
 }
