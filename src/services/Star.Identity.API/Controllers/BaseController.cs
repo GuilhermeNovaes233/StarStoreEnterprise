cac833e32f703596f6b9560ff0be6cfffe99b16b
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-09-02 22:51:08 -0300

    Criando projeto

---
+++ src/services/Star.Identity.API/Controllers/BaseController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Star.Identity.API.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (IsValid())
+            {
+                return Ok(result);
+            }
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>{
+                { "Messages", Errors.ToArray() }}));
+        }
+
+        //Coleta erros da nossa ModelState se houver
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                AddErrorProcessing(error.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool IsValid()
+        {
+            return !Errors.Any();
+        }
+
+        protected void AddErrorProcessing(string error)
+        {
+            Errors.Add(error);
+        }
+
+        protected void ClearErrorProcessing()
+        {
+            Errors.Clear();
+        }
+    }
+}
