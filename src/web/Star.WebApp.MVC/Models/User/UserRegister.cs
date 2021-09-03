c50a733d7bcf0045920975989fd1665399ff92eb
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-09-02 23:24:45 -0300

    Criando models de usuário - WebAPI

---
+++ src/web/Star.WebApp.MVC/Models/User/UserRegister.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Star.WebApp.MVC.Models.User
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DisplayName("Confirme sua senha")]
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmedPassword { get; set; }
+    }
+}
---
+++ src/web/Star.WebApp.MVC/Models/User/UserResponseLogin.cs
@@ -0,0 +1,11 @@
+﻿namespace Star.WebApp.MVC.Models.User
+{
+    public class UserResponseLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserToken UserToken { get; set; }
+
+        // public ResponseResult ResponseResult { get; set; }
+    }
+}
