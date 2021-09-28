c1977512d427a09257464375067c5b7bc3b1e436
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-09-27 22:53:36 -0300

    Adicionando arquivos de configuração - API Catalogo

---
+++ src/services/Star.Catalog.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Star.Catalog.API.Data;
+using Star.Catalog.API.Data.Repository;
+using Star.Catalog.API.Models;
+
+namespace Star.Catalog.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<CatalogContext>();
+        }
+    }
+}
