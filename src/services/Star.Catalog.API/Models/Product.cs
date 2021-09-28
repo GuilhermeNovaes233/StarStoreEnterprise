2e33cdd64ead86c912ebada0140066a2adc4a7a4
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-09-27 21:36:29 -0300

    Criando modelo Produto - API Catalogo

---
+++ src/services/Star.Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using Star.Core.DomainObjects;
+using System;
+
+namespace Star.Catalog.API.Models
+{
+    public class Product : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        //public decimal Value { get; set; }
+        public DateTime DataRegister { get; set; }
+        public string Image { get; set; }
+        public int QuantityStock { get; set; }
+    }
+}
