725c6d562fa5739fbed1c7cac15a3e49d92345a5
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-10-06 21:32:58 -0300

    Validação de acesso baseado em claims

--- src/services/Star.Catalog.API/Configuration/SwaggerConfig.cs
+++ src/services/Star.Catalog.API/Configuration/SwaggerConfig.cs
@@ -18,6 +18,31 @@ namespace Star.Catalog.API.Configuration
                     Contact = new OpenApiContact() { Name = "Guilherme Novaes", Email = "guilherme@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token Jwt desta maneira: Bearer {seuToken}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { 
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{ }
+                    }
+                });
             });
 
             return services;
--- src/services/Star.Catalog.API/Controllers/CatalogController.cs
+++ src/services/Star.Catalog.API/Controllers/CatalogController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Star.Catalog.API.Models;
+using Star.WebApi.Core.Identity;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Threading.Tasks;
 namespace Star.Catalog.API.Controllers
 {
     [ApiController]
+    [Authorize]
     public class CatalogController : Controller
     {
         private readonly IProductRepository _productRepository;
@@ -16,12 +19,14 @@ namespace Star.Catalog.API.Controllers
             _productRepository = productRepository;
         }
         
+        [AllowAnonymous]
         [HttpGet("catalog/products")]
         public async Task<IEnumerable<Product>> Index()
         {
             return await _productRepository.GetAll(); 
         }
 
+        [ClaimsAuthorize("Catalog", "Read")]
         [HttpGet("catalog/products/{id}")]
         public async Task<Product> ProductDetails(Guid id)
         {
