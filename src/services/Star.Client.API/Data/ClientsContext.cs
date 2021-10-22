ba88c6f8c08e462a67b9f8faa41ee2ec5c059a72
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-10-22 18:57:16 -0300

    Criando mapeamentos - API Cliente

---
+++ src/services/Star.Client.API/Data/ClientsContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Star.Client.API.Models;
+using Star.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Star.Client.API.Data
+{
+    public sealed class ClientsContext : DbContext, IUnitOfWork
+    {
+        public ClientsContext(DbContextOptions<ClientsContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Models.Client> Clients { get; set; }
+        public DbSet<Address> Address { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientsContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            var success = await base.SaveChangesAsync() > 0;
+            return success;
+        }
+    }
+}
