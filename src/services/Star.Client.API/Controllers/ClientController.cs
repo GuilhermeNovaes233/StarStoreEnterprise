019f1077151ea030e40912212027116c93e52ede
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-10-22 22:17:51 -0300

    Limpando camada do client e criando controller

---
+++ src/services/Star.Client.API/Controllers/ClientController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Star.Client.API.Application.Commands;
+using Star.Core.Mediator;
+using System;
+using System.Threading.Tasks;
+
+namespace Star.Client.API.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public ClientController(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpGet("clients")]
+        public async Task<IActionResult> Index()
+        {
+            var resultado = await _mediatorHandler.SendCommand(
+                new RegisterClientCommand(Guid.NewGuid(), "Guilherme", "guilherme@gmail.com", "661.388.600-98"));
+
+            //return CustomResponse(resultado);
+
+            throw new NotImplementedException();
+        }
+    }
+}
